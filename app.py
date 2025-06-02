7051691f0f3ae6c5a47002d791951841b9ef5759
Valtteri Antikainen <vantikaine@gmail.com>
2025-06-02 21:56:42 +0300

    Actually fixed data loading

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from data.config import API, API_SECRET, API_MEXC, API_SECRET_MEXC, API_BINANCE,
 app = Flask(__name__)
 CORS(app)
 
+total_cash = 0
 cash_balance = 0
 incoming_cash = []
 spending_limit = 0
@@ -23,32 +24,33 @@ mexc_client = futures.HTTP(api_key=API_MEXC, api_secret=API_SECRET_MEXC)
 
 binance_client = UMFutures(API_BINANCE, API_SECRET_BINANCE)
 
-def save_data():
-    global cash_balance, incoming_cash, spending_limit, crypto_total, transactions
+def save_data(data):
     with open('data/data.json', 'w') as f:
-        json.dump({
-            'cash_balance': cash_balance,
-            'incoming_cash': incoming_cash,
-            'spending_limit': spending_limit,
-            'crypto_total': crypto_total,
-            'transactions': transactions
-        }, f, ensure_ascii=False, indent=4)
+        json.dump(data, f, ensure_ascii=False, indent=4)
 
 def load_data():
     if os.path.exists('data/data.json'):
         with open('data/data.json', 'r') as f:
             return json.load(f)
     else:
-        return None
+        return {
+            'cash_balance': 0,
+            'incoming_cash': [],
+            'spending_limit': 0,
+            'crypto_total': 0,
+            'transactions': []
+        }
 
 data = load_data()
 if data:
+    total_cash = data["total_cash"]
     cash_balance = data["cash_balance"]
     incoming_cash = data["incoming_cash"]
     spending_limit = data["spending_limit"]
     crypto_total = data["crypto_total"]
     transactions = data["transactions"]
 else:
+    total_cash = 0
     cash_balance = 0
     incoming_cash = []
     spending_limit = 0
@@ -61,115 +63,115 @@ def index():
 
 @app.route('/total', methods=['GET'])
 def total():
-    global cash_balance, crypto_total
-    total_cash = cash_balance + crypto_total
-    save_data()
-    return jsonify(total=total_cash)
+    data = load_data()
+    data["total_cash"] = data["cash_balance"] + data["crypto_total"]
+    save_data(data)
+    return jsonify(total=data["total_cash"])
 
 @app.route('/cash', methods=['GET'])
 def cash():
-    global cash_balance, incoming_cash, spending_limit
-    save_data()
+    data = load_data()
+    save_data(data)
     return jsonify(
-        cash=cash_balance,
-        incoming_cash=incoming_cash,
-        spending_limit=spending_limit
+        cash=data["cash_balance"],
+        incoming_cash=data["incoming_cash"],
+        spending_limit=data["spending_limit"]
     )
 
 @app.route('/crypto', methods=['GET'])
 def crypto():
-    global crypto_total
-    crypto_total = float(bybit_client.get_wallet_balance(accountType='UNIFIED', currency='USDT')['result']['list'][0]['totalEquity'])
-    crypto_total += float(mexc_client.asset(currency='USDT')['data']['equity'])
+    data = load_data()
+    data["crypto_total"] = float(bybit_client.get_wallet_balance(accountType='UNIFIED', currency='USDT')['result']['list'][0]['totalEquity'])
+    data["crypto_total"] += float(mexc_client.asset(currency='USDT')['data']['equity'])
     balance = binance_client.balance()
     for i in balance:
         if i['asset'] == 'BNFCR':
-            crypto_total += float(i['balance'])
+            data["crypto_total"] += float(i['balance'])
     
     eur_usd = float(bybit_client.get_tickers(category='spot', symbol='USDTEUR')["result"]["list"][0]["lastPrice"])
-    crypto_total = round(crypto_total * eur_usd, 2)
+    data["crypto_total"] = round(data["crypto_total"] * eur_usd, 2)
 
-    save_data()
-    return jsonify(crypto=crypto_total)
+    save_data(data)
+    return jsonify(crypto=data["crypto_total"])
 
 @app.route('/cash', methods=['POST'])
 def update_cash():
-    global cash_balance, crypto_total, incoming_cash, spending_limit, transactions
-    data = request.get_json()
-    amount = data.get('amount', 0)
-    cash_balance += amount
+    data = load_data()
+    data_js = request.get_json()
+    amount = data_js.get('amount', 0)
+    data["cash_balance"] += amount
     
-    for i in incoming_cash:
+    for i in data["incoming_cash"]:
         if i["date"] == date.today().strftime('%d.%m.%Y'):
-            cash_balance += i["amount"]
-            incoming_cash.remove(i)
+            data["cash_balance"] += i["amount"]
+            data["incoming_cash"].remove(i)
     
-    if cash_balance < 0:
-        cash_balance = 0
+    if data["cash_balance"] < 0:
+        data["cash_balance"] = 0
     
-    crypto_total = float(bybit_client.get_wallet_balance(accountType='UNIFIED', currency='USDT')['result']['list'][0]['totalEquity'])
-    crypto_total += float(mexc_client.asset(currency='USDT')['data']['equity'])
+    data["crypto_total"] = float(bybit_client.get_wallet_balance(accountType='UNIFIED', currency='USDT')['result']['list'][0]['totalEquity'])
+    data["crypto_total"] += float(mexc_client.asset(currency='USDT')['data']['equity'])
     balance = binance_client.balance()
     for i in balance:
         if i['asset'] == 'BNFCR':
-            crypto_total += float(i['balance'])
+            data["crypto_total"] += float(i['balance'])
 
     eur_usd = float(bybit_client.get_tickers(category='spot', symbol='USDTEUR')["result"]["list"][0]["lastPrice"])
-    crypto_total = round(crypto_total * eur_usd, 2)
+    data["crypto_total"] = round(data["crypto_total"] * eur_usd, 2)
 
-    save_data()    
-    return jsonify(cash=cash_balance, crypto=crypto_total, total=cash_balance + crypto_total, incoming_cash=incoming_cash, spending_limit=spending_limit, transactions=transactions)
+    save_data(data)    
+    return jsonify(cash=data["cash_balance"], crypto=data["crypto_total"], total=data["cash_balance"] + data["crypto_total"], incoming_cash=data["incoming_cash"], spending_limit=data["spending_limit"], transactions=data["transactions"])
 
 @app.route('/reset', methods=['POST'])
 def reset_cash():
-    global cash_balance, incoming_cash, spending_limit, crypto_total
-    cash_balance = 0
-    total_cash = crypto_total
-    spending_limit = 0
-    save_data()
-    return jsonify(total=total_cash, cash=cash_balance, spending_limit=spending_limit)
+    data = load_data()
+    data["cash_balance"] = 0
+    data["total_cash"] = data["crypto_total"]
+    data["spending_limit"] = 0
+    save_data(data)
+    return jsonify(total=data["total_cash"], cash=data["cash_balance"], spending_limit=data["spending_limit"])
 
 @app.route('/incoming_cash', methods=['POST'])
 def add_incoming_cash():
-    global incoming_cash
-    data = request.get_json()
-    amount = data.get('amount', 0)
-    date_received = data.get('date', date.today().strftime('%d.%m.%Y'))
-    incoming_cash.append({
+    data = load_data()
+    data_js = request.get_json()
+    amount = data_js.get('amount', 0)
+    date_received = data_js.get('date', date.today().strftime('%d.%m.%Y'))
+    data["incoming_cash"].append({
         'amount': amount,
         'date': date_received
     })
-    save_data()
-    return jsonify(incoming_cash=incoming_cash)
+    save_data(data)
+    return jsonify(incoming_cash=data["incoming_cash"])
 
 @app.route('/spending_limit', methods=['POST'])
 def set_spending_limit():
-    global spending_limit
-    data = request.get_json()
-    spending_limit = data.get('limit', 0)
-    save_data()
-    return jsonify(spending_limit=spending_limit)
+    data = load_data()
+    data_js = request.get_json()
+    data["spending_limit"] = data_js.get('limit', 0)
+    save_data(data)
+    return jsonify(spending_limit=data["spending_limit"])
 
 @app.route('/transactions', methods=['POST'])
 def add_transaction():
-    global cash_balance, transactions
-    data = request.get_json()
-    tx_amount = data.get('amount', 0)
+    data = load_data()
+    data_js = request.get_json()
+    tx_amount = data_js.get('amount', 0)
     tx_date = date.today().strftime('%d.%m.%Y')
-    tx_text = data.get('text', '')
+    tx_text = data_js.get('text', '')
 
-    cash_balance -= tx_amount
-    if cash_balance < 0:
+    data["cash_balance"] -= tx_amount
+    if data["cash_balance"] < 0:
         return jsonify(error="Insufficient funds"), 400
     
-    transactions.insert(0, {
+    data["transactions"].insert(0, {
         'amount': tx_amount,
         'date': tx_date,
         'text': tx_text
     })
     
-    save_data()
-    return jsonify(tx_amount=tx_amount, tx_date=tx_date, tx_text=tx_text, transactions=transactions, cash=cash_balance)
+    save_data(data)
+    return jsonify(tx_amount=tx_amount, tx_date=tx_date, tx_text=tx_text, transactions=data["transactions"], cash=data["cash_balance"])
 
 
 if __name__ == '__main__':
